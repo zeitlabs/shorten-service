bbe3dab507b562516dcae010d7f020c6034005fb
YuLun Shih <rezacks@gmail.com>
2015-04-05 20:35:09 +0800

    Add code

---
+++ test/shorten.js
@@ -0,0 +1,83 @@
+var should = require('chai').should(),
+    Datastore = require('nedb'),
+    Shorten = require('../lib/shorten');
+
+describe('Shorten', function() {
+  var shorten = null,
+      code = null;
+
+  before(function() {
+    shorten = new Shorten({
+      db: new Datastore({filename: '/tmp/database.db', autoload: true })
+    });
+  });
+
+  after(function() {
+    try {
+      require('fs').unlinkSync('/tmp/database.db');
+    } catch (e) {}
+  });
+
+  describe('Prototype', function() {
+    it('should create an instance.', function() {
+      shorten.should.be.instanceOf(Shorten);
+    });
+  });
+
+  describe('Encode', function() {
+    it('should get saved result.', function(done) {
+      shorten.encode({
+        type: 'url',
+        value: 'http://eit.tw'
+      }, function(err, doc) {
+        // doc should look like this
+        // { code: 'HNqQvf',
+        //   data: { type: 'url', value: 'http://eit.tw' },
+        //   createdAt: '2015-04-05T08:14:47.761Z',
+        //   _id: 'Cq5j5vRAaK9OZHWD' }
+
+        should.not.exist(err);
+        should.exist(doc.data);
+        doc.code.should.have.length(6);
+        should.not.Throw(function() {
+          Date.parse(doc.createdAt);          
+        });
+        code = doc.code;
+        done();
+      });
+    });
+
+    it('should throw Exception if no object input.', function() {
+      should.Throw(function() {
+        shorten.encode();  
+      });
+    });
+  });
+
+  describe('Decode', function() {
+    it('should decode and return db object', function(done) {
+      shorten.decode(code, function(err, doc) {
+        if (err) {
+          return done(err)
+        }
+
+        if (doc === null) {
+          return done('code is not exist.');
+        }
+        doc.should.be.a.instanceOf(Object);
+        done();
+      });
+    });
+
+    it('should return null object if code is not found', function(done) {
+      shorten.decode('Not found', function(err, doc) {
+        if (err) {
+          return done(err)
+        }
+        should.not.exist(doc);
+        done();
+      });
+    });
+  });
+
+});
